dc310409b92cbc6bb9d62db9c4f89fff5720d413
Ufuk Beyaztas <58028887+UfukBeyaztas@users.noreply.github.com>
2023-07-15 16:02:41 +0300

    Create run_order.R

---
+++ run_order.R
@@ -0,0 +1,20 @@
+# Packages
+
+library(fda) 
+library(quantreg)
+library(matrixStats)
+library(Matrix)
+library(refund)
+library(nloptr)
+library(expm)
+library(here)
+
+# Load the source files
+run_order <- function(){
+  source(here("dgp1.R"))
+  source(here("dgp2.R"))
+  source(here("auxilary_functions_FPLS_FPCA.R"))
+  source(here("auxilary_functions_fpca.R"))
+  source(here("auxilary_functions_fpqr.R"))
+  source(here("auxilary_functions_pfqr.R"))
+}
