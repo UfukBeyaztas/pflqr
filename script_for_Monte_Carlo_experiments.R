505c612b77da2bf02c861c5fcd2afed71b14fc4b
Ufuk Beyaztas <58028887+UfukBeyaztas@users.noreply.github.com>
2023-07-15 13:52:06 +0300

    Update script_for_Monte_Carlo_experiments.R

--- script_for_Monte_Carlo_experiments.R
+++ script_for_Monte_Carlo_experiments.R
@@ -1,5 +1,7 @@
-source("auxilary_functions.R")
 source("auxilary_functions_fpca.R")
+source("auxilary_functions_FPLS_FPCA.R")
+source("auxilary_functions_fpqr.R")
+source("auxilary_functions_pfqr.R")
 source("dgp1.R")
 source("dgp2.R")
 
@@ -35,14 +37,45 @@ pffr.model <- pffr(Y ~ ff(X, xind = gpx), yind = gpy, data = data,
                    bs.int = list(bs = "ps", k = 15, m = c(2, 2)),
                    bs.yindex = list(bs = "ps", k = 15, m = c(2, 2)))
 
+# FPLS-based function-on-function linear quantile regression model
+X <- matrix(X, ncol = ncol(X))
+Y <- matrix(Y, ncol = ncol(Y))
+X.test <- matrix(X.test, ncol = ncol(X.test))
+
+# nbx, nby, and h were detedrimen via cross-validation
+fpqr.model <- fpqr(y=Y, x=X, xnew=X.test, h=4, tau=0.5, nby=5, nbx=5, gpy=gpy, gpx=gpx)
+fpqr.model.025 <- fpqr(y=Y, x=X, xnew=X.test, h=4, tau=0.025, nby=5, nbx=5, gpy=gpy, gpx=gpx)
+fpqr.model.975 <- fpqr(y=Y, x=X, xnew=X.test, h=4, tau=0.975, nby=5, nbx=5, gpy=gpy, gpx=gpx)
+
+# Functional partial least squares regression
+FVE.x = .99
+FVE.y = .99
+ridge.par = c(0)
+tune.method = 'CV'
+p.max = pUpper.compu(X, gpx, FVE.x, basis.name = "bspline")
+
+fplsr.model <- fAPLS(X, Y, gpx, gpy, X.test, ridge.par, p.max, tune = tune.method)
+
+# Functional principal component regression
+fpcr.model <- FPCR(X, Y, gpx, gpy, X.test, FVE.x, FVE.y, tune = T)
+
 
 # Predictions for a given test sample
 preds.pffr <- predict(pffr.model, newdata = list(X = X.test))
 preds.fpca <- fpca.model$pred
 preds.pffqr <- predict.fpenqr(object = pffqr.model, xnew = X.test)
+preds.fpqr <- fpqr.model$predicted.values
+preds.fplsr <- fplsr.model$y.pred
+preds.fpcr <- fpcr.model$y.pred
 
 preds.pffqr.025 <- predict.fpenqr(object = pffqr.model.025, xnew = X.test)
 preds.pffqr.975 <- predict.fpenqr(object = pffqr.model.975, xnew = X.test)
+
+preds.fpca.025 <- fpca.model.025$pred
+preds.fpca.975 <- fpca.model.975$pred
+
+preds.fpqr.025 <- fpqr.model.025$predicted.values
+preds.fpqr.975 <- fpqr.model.975$predicted.values
 
 # MSPE values
 # pffr
@@ -51,6 +84,12 @@ mean((Y.test-preds.pffr)^2) # 0.0006631121
 mean((Y.test-preds.fpca)^2) # 0.04839337
 # pflqr (proposed method)
 mean((Y.test-preds.pffqr)^2) # 7.352454e-05
+# fpqr
+mean((Y.test-preds.fpqr)^2) # 0.004559364
+# fplsr
+mean((Y.test-preds.fplsr)^2) # 0.02180835
+# fpcr
+mean((Y.test-preds.fpcr)^2) # 0.02470341
 
 
 # Regression parameter functions
@@ -76,6 +115,12 @@ pffrbeta.ts <- outer(pffrgridt, pffrgrids, beta)
 pffqr.alpha <- pffqr.model$b0.hat
 pffqr.beta <- pffqr.model$b.hat
 
+fpqr.alpha <- fpqr.model$b0hat
+fpqr.beta <- t(fpqr.model$bhat)
+
+fplsr.beta <- t(fplsr.model$Beta)
+fpcr.beta <- t(fpcr.model$Beta)
+
 # ISE(alpha)
 # pffr
 mean((alpha.t-pffralpha.t)^2) # 1.148243e-06
@@ -83,6 +128,8 @@ mean((alpha.t-pffralpha.t)^2) # 1.148243e-06
 mean((alpha.t-fpca.model$intercep)^2) # 0.004407539
 # pflqr
 mean((alpha.t-pffqr.alpha)^2) # 1.365669e-06
+# fpqr
+mean((alpha.t-fpqr.alpha)^2) # 0.00387519
 
 # ISE(beta)
 # pffr
@@ -91,19 +138,29 @@ mean((pffrbeta-pffrbeta.ts)^2) # 0.002531333
 mean((beta.ts-fpca.model$coef[[1]])^2) # 0.0196923
 # pflqr
 mean((beta.ts-pffqr.beta)^2) # 0.0003305728
+# fpqr
+mean((beta.ts-fpqr.beta)^2) # 0.002433181
+# fplsr
+mean((beta.ts-fplsr.beta)^2) # 0.0008759661
+# fpcr
+mean((beta.ts-fpcr.beta)^2) # 0.02142581
 
 # score and CPD values
 sco.cpd.fpca.sim <- matrix(, nrow = 100, ncol = 2)
 sco.cpd.pffqr.sim <- matrix(, nrow = 100, ncol = 2)
+sco.cpd.pfqr.sim <- matrix(, nrow = 100, ncol = 2)
 
 for(i in 1:100){
   sco.cpd.fpca.sim[i,] <- interval_score(Y.test[i,],fpca.model.025$pred[i,], fpca.model.975$pred[i,], 0.05)
   sco.cpd.pffqr.sim[i,] <- interval_score(Y.test[i,],preds.pffqr.025[i,], preds.pffqr.975[i,], 0.05)
+  sco.cpd.pfqr.sim[i,] <- interval_score(Y.test[i,],preds.fpqr.025[i,], preds.fpqr.975[i,], 0.05)
 }
 # flqr
 apply(sco.cpd.fpca.sim, 2, mean) # 7.8014272 0.9246667
 # pflqr
 apply(sco.cpd.pffqr.sim, 2, mean) # 0.07421137 0.05100000
+# fpqr
+apply(sco.cpd.pfqr.sim, 2, mean) # 1.9980595 0.9103333
 
 
 
@@ -141,22 +198,59 @@ pffr.model <- pffr(Y ~ ff(X, xind = gpx), yind = gpy, data = data,
                    bs.int = list(bs = "ps", k = 15, m = c(2, 2)),
                    bs.yindex = list(bs = "ps", k = 15, m = c(2, 2)))
 
+# FPLS-based function-on-function linear quantile regression model
+X <- matrix(X, ncol = ncol(X))
+Y <- matrix(Y, ncol = ncol(Y))
+X.test <- matrix(X.test, ncol = ncol(X.test))
+
+# nbx, nby, and h were detedrimen via cross-validation
+fpqr.model <- fpqr(y=Y, x=X, xnew=X.test, h=4, tau=0.5, nby=10, nbx=10, gpy=gpy, gpx=gpx)
+fpqr.model.025 <- fpqr(y=Y, x=X, xnew=X.test, h=4, tau=0.025, nby=10, nbx=10, gpy=gpy, gpx=gpx)
+fpqr.model.975 <- fpqr(y=Y, x=X, xnew=X.test, h=4, tau=0.975, nby=10, nbx=10, gpy=gpy, gpx=gpx)
+
+# Functional partial least squares regression
+FVE.x = .99
+FVE.y = .99
+ridge.par = c(0)
+tune.method = 'CV'
+p.max = pUpper.compu(X, gpx, FVE.x, basis.name = "bspline")
+
+fplsr.model <- fAPLS(X, Y, gpx, gpy, X.test, ridge.par, p.max, tune = tune.method)
+
+# Functional principal component regression
+fpcr.model <- FPCR(X, Y, gpx, gpy, X.test, FVE.x, FVE.y, tune = T)
+
 
 # Predictions for a given test sample
 preds.pffr <- predict(pffr.model, newdata = list(X = X.test))
 preds.fpca <- fpca.model$pred
 preds.pffqr <- predict.fpenqr(object = pffqr.model, xnew = X.test)
+preds.fpqr <- fpqr.model$predicted.values
+preds.fplsr <- fplsr.model$y.pred
+preds.fpcr <- fpcr.model$y.pred
 
 preds.pffqr.025 <- predict.fpenqr(object = pffqr.model.025, xnew = X.test)
 preds.pffqr.975 <- predict.fpenqr(object = pffqr.model.975, xnew = X.test)
 
+preds.fpca.025 <- fpca.model.025$pred
+preds.fpca.975 <- fpca.model.975$pred
+
+preds.fpqr.025 <- fpqr.model.025$predicted.values
+preds.fpqr.975 <- fpqr.model.975$predicted.values
+
 # MSPE values
 # pffr
-mean((Y.test-preds.pffr)^2) # 0.1244713
+mean((Y.test-preds.pffr)^2) # 0.1602355
 # flqr
-mean((Y.test-preds.fpca)^2) # 0.1378173
+mean((Y.test-preds.fpca)^2) # 0.1668938
 # pflqr (proposed method)
-mean((Y.test-preds.pffqr)^2) # 0.01161558
+mean((Y.test-preds.pffqr)^2) # 0.05797657
+# fpqr
+mean((Y.test-preds.fpqr)^2) # 0.1080985
+# fplsr
+mean((Y.test-preds.fplsr)^2) # 0.1725928
+# fpcr
+mean((Y.test-preds.fpcr)^2) # 0.2075199
 
 
 # Regression parameter functions
@@ -182,32 +276,50 @@ pffrbeta.ts <- outer(pffrgridt, pffrgrids, beta)
 pffqr.alpha <- pffqr.model$b0.hat
 pffqr.beta <- pffqr.model$b.hat
 
+fpqr.alpha <- fpqr.model$b0hat
+fpqr.beta <- t(fpqr.model$bhat)
+
+fplsr.beta <- t(fplsr.model$Beta)
+fpcr.beta <- t(fpcr.model$Beta)
+
 # ISE(alpha)
 # pffr
-mean((alpha.t-pffralpha.t)^2) # 0.01156349
+mean((alpha.t-pffralpha.t)^2) # 0.006997925
 # flqr
-mean((alpha.t-fpca.model$intercep)^2) # 0.06153902
+mean((alpha.t-fpca.model$intercep)^2) # 0.06176412
 # pflqr
-mean((alpha.t-pffqr.alpha)^2) # 0.0003737888
+mean((alpha.t-pffqr.alpha)^2) # 0.004339547
+# fpqr
+mean((alpha.t-fpqr.alpha)^2) # 0.04303604
 
 # ISE(beta)
 # pffr
-mean((pffrbeta-pffrbeta.ts)^2) # 3.817366
+mean((pffrbeta-pffrbeta.ts)^2) # 3.580498
 # flqr
-mean((beta.ts-fpca.model$coef[[1]])^2) # 3.586012
+mean((beta.ts-fpca.model$coef[[1]])^2) # 3.633358
 # pflqr
-mean((beta.ts-pffqr.beta)^2) # 1.403133
+mean((beta.ts-pffqr.beta)^2) # 2.028303
+# fpqr
+mean((beta.ts-fpqr.beta)^2) # 2.048421
+# fplsr
+mean((beta.ts-fplsr.beta)^2) # 3.589995
+# fpcr
+mean((beta.ts-fpcr.beta)^2) # 3.607519
 
 # score and CPD values
 sco.cpd.fpca.sim <- matrix(, nrow = 100, ncol = 2)
 sco.cpd.pffqr.sim <- matrix(, nrow = 100, ncol = 2)
+sco.cpd.pfqr.sim <- matrix(, nrow = 100, ncol = 2)
 
 for(i in 1:100){
   sco.cpd.fpca.sim[i,] <- interval_score(Y.test[i,],fpca.model.025$pred[i,], fpca.model.975$pred[i,], 0.05)
   sco.cpd.pffqr.sim[i,] <- interval_score(Y.test[i,],preds.pffqr.025[i,], preds.pffqr.975[i,], 0.05)
+  sco.cpd.pfqr.sim[i,] <- interval_score(Y.test[i,],preds.fpqr.025[i,], preds.fpqr.975[i,], 0.05)
 }
 # flqr
-apply(sco.cpd.fpca.sim, 2, mean) # 17.93953  0.94700
+apply(sco.cpd.fpca.sim, 2, mean) # 18.1760561  0.7111667
 # pflqr
-apply(sco.cpd.pffqr.sim, 2, mean) # 1.2187493 0.1718333
+apply(sco.cpd.pffqr.sim, 2, mean) # 6.1430686 0.7383333
+# fpqr
+apply(sco.cpd.pfqr.sim, 2, mean) # 2.145448 0.097500
 
