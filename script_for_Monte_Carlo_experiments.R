5ccbcac7caa125f09e36e4ea491e7582aed82d88
Ufuk Beyaztas <58028887+UfukBeyaztas@users.noreply.github.com>
2023-11-08 14:58:46 +0300

    Update script_for_Monte_Carlo_experiments.R

--- script_for_Monte_Carlo_experiments.R
+++ script_for_Monte_Carlo_experiments.R
@@ -76,19 +76,19 @@ preds.fpca.975 <- fpca.model.975$pred
 preds.fpqr.025 <- fpqr.model.025$predicted.values
 preds.fpqr.975 <- fpqr.model.975$predicted.values
 
-# MSPE values
+# RMSPE values
 # pffr
-mean((Y.test-preds.pffr)^2) # 0.0006631121
+rmspe(Y.test, preds.pffr,  gpy)  # 0.7375234
 # flqr
-mean((Y.test-preds.fpca)^2) # 0.04839337
+rmspe(Y.test, preds.fpca, gpy)  # 10.28026
 # pflqr (proposed method)
-mean((Y.test-preds.pffqr)^2) # 7.352454e-05
+rmspe(Y.test, preds.pffqr, gpy)  # 0.2452742
 # fpqr
-mean((Y.test-preds.fpqr)^2) # 0.004559364
+rmspe(Y.test, preds.fpqr, gpy)  # 3.062987
 # fplsr
-mean((Y.test-preds.fplsr)^2) # 0.02180835
+rmspe(Y.test, preds.fplsr, gpy)  # 6.923498
 # fpcr
-mean((Y.test-preds.fpcr)^2) # 0.02470341
+rmspe(Y.test, preds.fpcr, gpy)  # 7.175906
 
 
 # Regression parameter functions
@@ -120,29 +120,29 @@ fpqr.beta <- t(fpqr.model$bhat)
 fplsr.beta <- t(fplsr.model$Beta)
 fpcr.beta <- t(fpcr.model$Beta)
 
-# ISE(alpha)
+# RRISPEE(alpha)
 # pffr
-mean((alpha.t-pffralpha.t)^2) # 1.148243e-06
+reispe.a(alpha.t, pffralpha.t, gpy)  # 0.06648273
 # flqr
-mean((alpha.t-fpca.model$intercep)^2) # 0.004407539
+reispe.a(alpha.t, fpca.model$intercep, gpy)  # 4.226501
 # pflqr
-mean((alpha.t-pffqr.alpha)^2) # 1.365669e-06
+reispe.a(alpha.t, pffqr.alpha, gpy) # 0.07477094
 # fpqr
-mean((alpha.t-fpqr.alpha)^2) # 0.00387519
+reispe.a(alpha.t, fpqr.alpha, gpy)  # 3.964192
 
-# ISE(beta)
+# RRISPEE(beta)
 # pffr
-mean((pffrbeta-pffrbeta.ts)^2) # 0.002531333
+rispe.b(pffrbeta, pffrbeta.ts, pffrgridt, pffrgrids) # 2.422286
 # flqr
-mean((beta.ts-fpca.model$coef[[1]])^2) # 0.0196923
+rispe.b(beta.ts, fpca.model$coef[[1]], gpx, gpy) # 6.841824
 # pflqr
-mean((beta.ts-pffqr.beta)^2) # 0.0003305728
+rispe.b(beta.ts, pffqr.beta, gpx, gpy) # 0.8054657
 # fpqr
-mean((beta.ts-fpqr.beta)^2) # 0.002433181
+rispe.b(beta.ts, fpqr.beta, gpx, gpy) # 2.274095
 # fplsr
-mean((beta.ts-fplsr.beta)^2) # 0.0008759661
+rispe.b(beta.ts, fplsr.beta, gpx, gpy) # 1.453825
 # fpcr
-mean((beta.ts-fpcr.beta)^2) # 0.02142581
+rispe.b(beta.ts, fpcr.beta, gpx, gpy) # 7.146398
 
 # score and CPD values
 sco.cpd.fpca.sim <- matrix(, nrow = 100, ncol = 2)
@@ -237,19 +237,19 @@ preds.fpca.975 <- fpca.model.975$pred
 preds.fpqr.025 <- fpqr.model.025$predicted.values
 preds.fpqr.975 <- fpqr.model.975$predicted.values
 
-# MSPE values
+# RMSPE values
 # pffr
-mean((Y.test-preds.pffr)^2) # 0.1602355
+rmspe(Y.test, preds.pffr,  gpy)  # 21.60608
 # flqr
-mean((Y.test-preds.fpca)^2) # 0.1668938
+rmspe(Y.test, preds.fpca, gpy)  # 22.2455
 # pflqr (proposed method)
-mean((Y.test-preds.pffqr)^2) # 0.05797657
+rmspe(Y.test, preds.pffqr, gpy)  # 13.69135
 # fpqr
-mean((Y.test-preds.fpqr)^2) # 0.1080985
+rmspe(Y.test, preds.fpqr, gpy)  # 18.85225
 # fplsr
-mean((Y.test-preds.fplsr)^2) # 0.1725928
+rmspe(Y.test, preds.fplsr, gpy)  # 24.14197
 # fpcr
-mean((Y.test-preds.fpcr)^2) # 0.2075199
+rmspe(Y.test, preds.fpcr, gpy)  # 25.0166
 
 
 # Regression parameter functions
@@ -281,29 +281,29 @@ fpqr.beta <- t(fpqr.model$bhat)
 fplsr.beta <- t(fplsr.model$Beta)
 fpcr.beta <- t(fpcr.model$Beta)
 
-# ISE(alpha)
+# RRISPEE(alpha)
 # pffr
-mean((alpha.t-pffralpha.t)^2) # 0.006997925
+reispe.a(alpha.t, pffralpha.t, gpy)  # 5.855526
 # flqr
-mean((alpha.t-fpca.model$intercep)^2) # 0.06176412
+reispe.a(alpha.t, fpca.model$intercep, gpy)  # 16.87415
 # pflqr
-mean((alpha.t-pffqr.alpha)^2) # 0.004339547
+reispe.a(alpha.t, pffqr.alpha, gpy) # 4.450031
 # fpqr
-mean((alpha.t-fpqr.alpha)^2) # 0.04303604
+reispe.a(alpha.t, fpqr.alpha, gpy)  # 14.15366
 
-# ISE(beta)
+# RRISPEE(beta)
 # pffr
-mean((pffrbeta-pffrbeta.ts)^2) # 3.580498
+rispe.b(pffrbeta, pffrbeta.ts, pffrgridt, pffrgrids) # 340.1204
 # flqr
-mean((beta.ts-fpca.model$coef[[1]])^2) # 3.633358
+rispe.b(beta.ts, fpca.model$coef[[1]], gpx, gpy) # 95.26373
 # pflqr
-mean((beta.ts-pffqr.beta)^2) # 2.028303
+rispe.b(beta.ts, pffqr.beta, gpx, gpy) # 70.40651
 # fpqr
-mean((beta.ts-fpqr.beta)^2) # 2.048421
+rispe.b(beta.ts, fpqr.beta, gpx, gpy) # 71.1427
 # fplsr
-mean((beta.ts-fplsr.beta)^2) # 3.589995
+rispe.b(beta.ts, fplsr.beta, gpx, gpy) # 94.68226
 # fpcr
-mean((beta.ts-fpcr.beta)^2) # 3.607519
+rispe.b(beta.ts, fpcr.beta, gpx, gpy) # 94.91387
 
 # score and CPD values
 sco.cpd.fpca.sim <- matrix(, nrow = 100, ncol = 2)
@@ -322,3 +322,161 @@ apply(sco.cpd.pffqr.sim, 2, mean) # 6.1430686 0.7383333
 # fpqr
 apply(sco.cpd.pfqr.sim, 2, mean) # 2.145448 0.097500
 
+
+
+
+
+
+# An example run under DGP-III
+
+set.seed(123)
+data <- dgp3(n = 50, gpy = gpy, gpx = gpx)
+Y <- data$Y
+X <- data$X
+
+data.test <- dgp3(n = 100, gpy = gpy, gpx = gpx)
+X.test <- data.test$X
+Y.test <- data.test$Ytrue
+
+# Proposed method
+pffqr.model <- fpenqr(y=Y, x=X, tau=0.5, nby=15, nbx=15, nb0=15, gpy=gpy, gpx=gpx)
+pffqr.model.025 <- fpenqr(y=Y, x=X, tau=0.025, nby=15, nbx=15, nb0=15, gpy=gpy, gpx=gpx)
+pffqr.model.975 <- fpenqr(y=Y, x=X, tau=0.975, nby=15, nbx=15, nb0=15, gpy=gpy, gpx=gpx)
+
+# FPCA-based function-on-function linear quantile regression model
+fpca.model <- fpca(fY=Y, fX=list(X), fX_test=list(X.test), tau=0.5, gpy=gpy, gpx=list(gpx),
+                   fnbasisY=15, fnbasisX=15, fncomp_Y=5, fncomp_X=5)
+fpca.model.025 <- fpca(fY=Y, fX=list(X), fX_test=list(X.test), tau=0.025, gpy=gpy, gpx=list(gpx),
+                       fnbasisY=15, fnbasisX=15, fncomp_Y=5, fncomp_X=5)
+fpca.model.975 <- fpca(fY=Y, fX=list(X), fX_test=list(X.test), tau=0.975, gpy=gpy, gpx=list(gpx),
+                       fnbasisY=15, fnbasisX=15, fncomp_Y=5, fncomp_X=5)
+
+# pffr method
+pffr.model <- pffr(Y ~ ff(X, xind = gpx), yind = gpy, data = data, 
+                   bs.int = list(bs = "ps", k = 15, m = c(2, 2)),
+                   bs.yindex = list(bs = "ps", k = 15, m = c(2, 2)))
+
+# FPLS-based function-on-function linear quantile regression model
+X <- matrix(X, ncol = ncol(X))
+Y <- matrix(Y, ncol = ncol(Y))
+X.test <- matrix(X.test, ncol = ncol(X.test))
+
+# nbx, nby, and h were detedrimen via cross-validation
+fpqr.model <- fpqr(y=Y, x=X, xnew=X.test, h=4, tau=0.5, nby=10, nbx=10, gpy=gpy, gpx=gpx)
+fpqr.model.025 <- fpqr(y=Y, x=X, xnew=X.test, h=4, tau=0.025, nby=10, nbx=10, gpy=gpy, gpx=gpx)
+fpqr.model.975 <- fpqr(y=Y, x=X, xnew=X.test, h=4, tau=0.975, nby=10, nbx=10, gpy=gpy, gpx=gpx)
+
+# Functional partial least squares regression
+FVE.x = .99
+FVE.y = .99
+ridge.par = c(0)
+tune.method = 'CV'
+p.max = pUpper.compu(X, gpx, FVE.x, basis.name = "bspline")
+
+fplsr.model <- fAPLS(X, Y, gpx, gpy, X.test, ridge.par, p.max, tune = tune.method)
+
+# Functional principal component regression
+fpcr.model <- FPCR(X, Y, gpx, gpy, X.test, FVE.x, FVE.y, tune = T)
+
+
+# Predictions for a given test sample
+preds.pffr <- predict(pffr.model, newdata = list(X = X.test))
+preds.fpca <- fpca.model$pred
+preds.pffqr <- predict.fpenqr(object = pffqr.model, xnew = X.test)
+preds.fpqr <- fpqr.model$predicted.values
+preds.fplsr <- fplsr.model$y.pred
+preds.fpcr <- fpcr.model$y.pred
+
+preds.pffqr.025 <- predict.fpenqr(object = pffqr.model.025, xnew = X.test)
+preds.pffqr.975 <- predict.fpenqr(object = pffqr.model.975, xnew = X.test)
+
+preds.fpca.025 <- fpca.model.025$pred
+preds.fpca.975 <- fpca.model.975$pred
+
+preds.fpqr.025 <- fpqr.model.025$predicted.values
+preds.fpqr.975 <- fpqr.model.975$predicted.values
+
+# RMSPE values
+# pffr
+rmspe(Y.test, preds.pffr,  gpy)  # 37.60322
+# flqr
+rmspe(Y.test, preds.fpca, gpy)  # 42.14422
+# pflqr (proposed method)
+rmspe(Y.test, preds.pffqr, gpy)  # 32.55478
+# fpqr
+rmspe(Y.test, preds.fpqr, gpy)  # 35.00295
+# fplsr
+rmspe(Y.test, preds.fplsr, gpy)  # 68.37568
+# fpcr
+rmspe(Y.test, preds.fpcr, gpy)  # 69.03762
+
+
+# Regression parameter functions
+alpha <- function(t) 2 * sin(4*pi*t)
+beta <- function(t, s) {
+  4 * cos(4 * pi * t) * sin(4*pi * s)
+}
+
+alpha.t <- alpha(gpy)
+beta.ts <- outer(gpy, gpx, beta)
+
+# Estimates of the regression parameter function by pffr
+pffrfit <- plot(pffr.model, select = 0)
+pffralpha <- pffrfit[[1]]$fit + pffr.model$coefficients[1]
+pffralpgrid <- pffrfit[[1]]$x
+pffrbetafit <- pffrfit[[2]]$fit
+pffrgrids <- pffrfit[[2]]$x
+pffrgridt <- pffrfit[[2]]$y
+pffrbeta <- t(matrix(pffrbetafit, length(pffrgrids), length(pffrgridt)))
+pffralpha.t <- approx(pffralpgrid, pffralpha, gpy)$y 
+pffrbeta.ts <- outer(pffrgridt, pffrgrids, beta)
+
+pffqr.alpha <- pffqr.model$b0.hat
+pffqr.beta <- pffqr.model$b.hat
+
+fpqr.alpha <- fpqr.model$b0hat
+fpqr.beta <- t(fpqr.model$bhat)
+
+fplsr.beta <- t(fplsr.model$Beta)
+fpcr.beta <- t(fpcr.model$Beta)
+
+# RRISPEE(alpha)
+# pffr
+reispe.a(alpha.t, pffralpha.t, gpy)  # 32.8456
+# flqr
+reispe.a(alpha.t, fpca.model$intercep, gpy)  # 40.64746
+# pflqr
+reispe.a(alpha.t, pffqr.alpha, gpy) # 30.75653
+# fpqr
+reispe.a(alpha.t, fpqr.alpha, gpy)  # 33.56794
+
+# RRISPEE(beta)
+# pffr
+rispe.b(pffrbeta, pffrbeta.ts, pffrgridt, pffrgrids) # 344.1322
+# flqr
+rispe.b(beta.ts, fpca.model$coef[[1]], gpx, gpy) # 95.23561
+# pflqr
+rispe.b(beta.ts, pffqr.beta, gpx, gpy) # 71.52271
+# fpqr
+rispe.b(beta.ts, fpqr.beta, gpx, gpy) # 77.46408
+# fplsr
+rispe.b(beta.ts, fplsr.beta, gpx, gpy) # 94.82672
+# fpcr
+rispe.b(beta.ts, fpcr.beta, gpx, gpy) # 94.92413
+
+# score and CPD values
+sco.cpd.fpca.sim <- matrix(, nrow = 100, ncol = 2)
+sco.cpd.pffqr.sim <- matrix(, nrow = 100, ncol = 2)
+sco.cpd.pfqr.sim <- matrix(, nrow = 100, ncol = 2)
+
+for(i in 1:100){
+  sco.cpd.fpca.sim[i,] <- interval_score(Y.test[i,],fpca.model.025$pred[i,], fpca.model.975$pred[i,], 0.05)
+  sco.cpd.pffqr.sim[i,] <- interval_score(Y.test[i,],preds.pffqr.025[i,], preds.pffqr.975[i,], 0.05)
+  sco.cpd.pfqr.sim[i,] <- interval_score(Y.test[i,],preds.fpqr.025[i,], preds.fpqr.975[i,], 0.05)
+}
+# flqr
+apply(sco.cpd.fpca.sim, 2, mean) # 27.56852  0.85300
+# pflqr
+apply(sco.cpd.pffqr.sim, 2, mean) # 16.01658 0.95000
+# fpqr
+apply(sco.cpd.pfqr.sim, 2, mean) # 4.9388578 0.1783333
