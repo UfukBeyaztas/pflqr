73d218b02fb558d2eb2c3d2db7a4eb0b631cda50
Ufuk Beyaztas <58028887+UfukBeyaztas@users.noreply.github.com>
2023-11-08 14:29:27 +0300

    Update run_order.R

--- run_order.R
+++ run_order.R
@@ -13,6 +13,7 @@ library(here)
 run_order <- function(){
   source(here("dgp1.R"))
   source(here("dgp2.R"))
+  source(here("dgp3.R"))
   source(here("auxilary_functions_FPLS_FPCA.R"))
   source(here("auxilary_functions_fpca.R"))
   source(here("auxilary_functions_fpqr.R"))
